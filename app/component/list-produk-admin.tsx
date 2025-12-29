edc920079a64b26505f3bc82d97bc5f5ee550dcf
firrrman <firmanhakim11704@gmail.com>
2025-12-29 15:37:59 +0700

    fixed button

--- app/component/list-produk-admin.tsx
+++ app/component/list-produk-admin.tsx
@@ -45,7 +45,7 @@ export default function ProdukListAdmin({
     <main className="p-4 md:p-6 overflow-y-auto">
       {/* Header */}
       <div className="mb-6">
-        <div className="flex items-center justify-between mb-6">
+        <div className="flex items-center justify-between flex-wrap gap-3 mb-6">
           <div>
             <h1 className="text-2xl font-bold text-slate-800">
               Manajemen Produk
