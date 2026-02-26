29ed8ecfba0d9dd496b0fc6abb5838b60a0e0b1c
firrrman <firmanhakim11704@gmail.com>
2026-02-26 12:59:56 +0700

    update fixed in header

--- app/component/header.tsx
+++ app/component/header.tsx
@@ -100,7 +100,7 @@ const CardNav: React.FC<CardNavProps> = ({
     tl.to(
       cardsRef.current,
       { y: 0, opacity: 1, duration: 0.4, ease, stagger: 0.08 },
-      "-=0.1"
+      "-=0.1",
     );
 
     return tl;
@@ -165,7 +165,7 @@ const CardNav: React.FC<CardNavProps> = ({
 
   return (
     <div
-      className={`card-nav-container absolute left-1/2 -translate-x-1/2 w-[90%] max-w-200 z-99 top-[1.2em] md:top-[2em] ${className}`}
+      className={`card-nav-container fixed left-1/2 -translate-x-1/2 w-[90%] max-w-200 z-99 top-[1.2em] md:top-[2em] ${className}`}
     >
       <nav
         ref={navRef}
